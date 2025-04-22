c0033dd6a4e0994d8afb61a2ad4abc4d9935fc7c
Oliver Eriksen <okebedrift@gmail.com>
2025-04-22 21:46:18 +0200

    fix: update julianWeekdayMapper to align with weekday

--- calendar/utils/mapJulianWeekday.ts
+++ calendar/utils/mapJulianWeekday.ts
@@ -1,16 +1,16 @@
 const julianWeekdayMapper: Record<number, string> = {
-    0: "Sunday",
-    1: "Monday",
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday",
-}
+   1: "Sunday",
+   2: "Monday",
+   3: "Tuesday",
+   4: "Wednesday",
+   5: "Thursday",
+   6: "Friday",
+   7: "Saturday",
+};
 
 export default function mapJulianWeekday(weekday: number): string {
-    if (weekday < 0 || weekday > 6) {
-        throw new Error("Invalid weekday number. Must be between 0 and 6.");
-    }
-    return julianWeekdayMapper[weekday];
+   if (weekday < 0 || weekday > 6) {
+      throw new Error("Invalid weekday number. Must be between 0 and 6.");
+   }
+   return julianWeekdayMapper[weekday];
 }
