6deb1c0c6bec0b6bdd4bba82c8c8fcee324b5a52
Oliver Eriksen <okebedrift@gmail.com>
2025-04-22 21:45:13 +0200

    feat: handle button submit with weekday return

--- calendar/app/page.tsx
+++ calendar/app/page.tsx
@@ -3,66 +3,90 @@ import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import returnWeekday from "@/utils/returnWeekday";
 
 export default function Home() {
-  const [day, setDay] = useState(4);
-  const [month, setMonth] = useState(4);
-  const [year, setYear] = useState(2025);
+   const [day, setDay] = useState(4);
+   const [month, setMonth] = useState(4);
+   const [year, setYear] = useState(2025);
 
-  const isDayValid = day >= 1 && day <= 31;
-  const isMonthValid = month >= 1 && month <= 12;
-  const isYearValid = year >= -1000 && year <= 2030;
+   const isDayValid = day >= 1 && day <= 31;
+   const isMonthValid = month >= 1 && month <= 12;
+   const isYearValid = year >= -1000 && year <= 2030;
 
-  const isFormValid = isDayValid && isMonthValid && isYearValid;
+   const isFormValid = isDayValid && isMonthValid && isYearValid;
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-2 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>Calendar</p>
-      <div className="flex flex-col gap-2">
-        <Label htmlFor="day">Day</Label>
-        <Input
-          type="number"
-          placeholder="Day"
-          id="day"
-          className="w-44"
-          min="1"
-          max="31"
-          value={day}
-          onChange={(e) => setDay(Number(e.target.value))}
-        />
-        {!isDayValid && (<p className="text-red-500 text-xs">Must be between 1-31</p>)}
+   const [weekday, setWeekday] = useState("");
+   const [calenderType, setCalenderType] = useState("");
+   const [dateExists, setDateExists] = useState("");
+
+   interface WeekdayReturnData {
+      weekday: string;
+      calenderType: string;
+      dateExists: boolean;
+   }
+
+   const handleSubmit = () => {
+      const returnData: WeekdayReturnData = returnWeekday(day, month, year);
+      setCalenderType(returnData.calenderType);
+      setWeekday(returnData.weekday);
+      setDateExists(returnData.dateExists ? "Yes" : "No");
+   };
+
+   return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-2 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+         <p>Calendar</p>
+         <div className="flex flex-col gap-2">
+            <Label htmlFor="day">Day</Label>
+            <Input
+               type="number"
+               placeholder="Day"
+               id="day"
+               className="w-44"
+               min="1"
+               max="31"
+               value={day}
+               onChange={(e) => setDay(Number(e.target.value))}
+            />
+            {!isDayValid && <p className="text-red-500 text-xs">Must be between 1-31</p>}
+         </div>
+         <div className="flex flex-col gap-2">
+            <Label htmlFor="month">Month</Label>
+            <Input
+               type="number"
+               placeholder="Month"
+               id="month"
+               className="w-44"
+               min="1"
+               max="12"
+               value={month}
+               onChange={(e) => setMonth(Number(e.target.value))}
+            />
+            {!isMonthValid && <p className="text-red-500 text-xs">Must be between 1-12</p>}
+         </div>
+         <div className="flex flex-col gap-2">
+            <Label htmlFor="year">Year</Label>
+            <Input
+               type="number"
+               placeholder="Year"
+               id="year"
+               className="w-44"
+               min="-1000"
+               max="2030"
+               value={year}
+               onChange={(e) => setYear(Number(e.target.value))}
+            />
+            {!isYearValid && <p className="text-red-500 text-xs">Must be between -1000-2030</p>}
+         </div>
+         <Button className="w-44" variant="default" disabled={!isFormValid} onClick={handleSubmit}>
+            Submit
+         </Button>
+
+         <div className="flex flex-col gap-2">
+            {weekday && <p className="text-sm">Weekday: {weekday}</p>}
+            {calenderType && <p className="text-sm">Calendar Type: {calenderType}</p>}
+            {dateExists && <p className="text-sm">Date Exists: {dateExists}</p>}
+         </div>
       </div>
-      <div className="flex flex-col gap-2">
-        <Label htmlFor="month">Month</Label>
-        <Input
-          type="number"
-          placeholder="Month"
-          id="month"
-          className="w-44"
-          min="1"
-          max="12"
-          value={month}
-          onChange={(e) => setMonth(Number(e.target.value))}
-        />
-        {!isMonthValid && (<p className="text-red-500 text-xs">Must be between 1-12</p>)}
-      </div>
-      <div className="flex flex-col gap-2">
-        <Label htmlFor="year">Year</Label>
-        <Input
-          type="number"
-          placeholder="Year"
-          id="year"
-          className="w-44"
-          min="-1000"
-          max="2030"
-          value={year}
-          onChange={(e) => setYear(Number(e.target.value))}
-        />
-        {!isYearValid && (<p className="text-red-500 text-xs">Must be between -1000-2030</p>)}
-      </div>
-      <Button className="w-44" variant="default" disabled={!isFormValid}>
-        Submit
-      </Button>
-    </div>
-  );
+   );
 }
