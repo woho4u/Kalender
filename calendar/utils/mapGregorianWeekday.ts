04b917e352764539dce79a46fdb781c747102b4f
Oliver Eriksen <okebedrift@gmail.com>
2025-04-22 21:46:29 +0200

    fix: correct mapping of weekdays

--- calendar/utils/mapGregorianWeekday.ts
+++ calendar/utils/mapGregorianWeekday.ts
@@ -1,16 +1,16 @@
 const numberToGregorianWeekdayMapper: Record<number, string> = {
-    0: "Saturday",
-    1: "Sunday",
-    2: "Monday",
-    3: "Tuesday",
-    4: "Wednesday",
-    5: "Thursday",
-    6: "Friday",
-}
+   1: "Monday",
+   2: "Tuesday",
+   3: "Wednesday",
+   4: "Thursday",
+   5: "Friday",
+   6: "Saturday",
+   7: "Sunday",
+};
 
 export default function mapGregorianWeekday(weekday: number): string {
-    if (weekday < 0 || weekday > 6) {
-        throw new Error("Invalid weekday number. Must be between 0 and 6.");
-    }
-    return numberToGregorianWeekdayMapper[weekday];
+   if (weekday < 0 || weekday > 6) {
+      throw new Error("Invalid weekday number. Must be between 0 and 6.");
+   }
+   return numberToGregorianWeekdayMapper[weekday];
 }
